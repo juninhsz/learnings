3768ba42849545db1b610df1db2715c3d5e0fdf5
juninhsz <juninh643@gmail.com>
2025-07-18 11:46:39 -0300

    Meu primeiro commit

---
+++ learn.js
@@ -0,0 +1,10 @@
+var idade, conclusao;
+idade = 15
+var maiorIdade;
+maiorIdade = 18
+
+conclusao = (idade >= maiorIdade) ? "maior idade":"menor idade"
+
+
+alert("O usuário tem"+ " " + conclusao)
+
