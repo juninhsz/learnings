4f6f207f4eefce5d8b644278f7a11c739b41355b
juninhsz <juninh643@gmail.com>
2025-07-21 11:49:21 -0300

    Coloquei em pratica algumas funções como de soma, converter real pra dolar e converter Fahrenheit

--- learn.js
+++ learn.js
@@ -8,3 +8,20 @@ conclusao = (idade >= maiorIdade) ? "maior idade":"menor idade"
 
 alert("O usuário tem"+ " " + conclusao)
 
+function realParaDolar (real, cotacaoDolar) {
+    return real * cotacaoDolar
+}
+var valoReal = 10;
+var cotacao = 5.57;
+ 
+var total = realParaDolar(valoReal, cotacao)
+
+alert("O valor em real é de " + valoReal +"R$" + " e o valor em dólar é de " + total + "U$")
+
+function soma (valor1, valor2) {
+    return valor1 + valor2
+}
+
+function paraCelsius (valorFahrenheit){
+    return (5/9) * (valorFahrenheit-32)
+}
